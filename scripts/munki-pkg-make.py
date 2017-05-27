c7de6c048a79f9bfdb60928ce074ac7d14546f1d
Jacob F. Grant <git@jacobfgrant.com>
2017-05-27 01:09:39 -0700

    Add options to run munki-pkg-make.py on other directories

--- scripts/munki-pkg-make.py
+++ scripts/munki-pkg-make.py
@@ -15,10 +15,12 @@ import sys
 import os
 import getopt
 import subprocess
-import plistlib
+import argparse
+import shutil
 
 
-def inPATH(exe):
+def inPath(exe):
+    """Check PATH environment for executable."""
     for path in os.environ["PATH"].split(os.pathsep):
         path = path.strip('"')
         exe_path = os.path.join(path, exe)
@@ -27,48 +29,127 @@ def inPATH(exe):
     return None
 
 
-def isPackage(pkgdir):
-    if not (os.path.isdir(os.path.join(pkgdir, 'payload')) or os.path.isdir(os.path.join(pkgdir, 'scripts'))):
+def isPackage(pkgDir):
+    """Check the contents of a directory for the files/directories required for munki-pkg."""
+    # Check for 'payload' or 'scripts' directories
+    if not (os.path.isdir(os.path.join(pkgDir, 'payload')) or
+            os.path.isdir(os.path.join(pkgDir, 'scripts'))):
+        print >> sys.stderr, "munki-pkg-make:", pkgDir, "missing payload and scripts directories"
         return False
-    
-    if not os.path.isfile(os.path.join(pkgdir, 'Bom.txt')):
+        
+    # Check for 'Bom.txt'
+    if not os.path.isfile(os.path.join(pkgDir, 'Bom.txt')):
+        print >> sys.stderr, "munki-pkg-make:", pkgDir, "missing Bom.txt"
         return False
-    
+        
+    # Check for 'build-info[.plist][.json][.yml]'
     for ext in ['.plist', '.json', '.yml']:
-        if os.path.isfile(os.path.join(pkgdir, ('build-info' + ext))):
+        if os.path.isfile(os.path.join(pkgDir, ('build-info' + ext))):
             return True
     
+    print >> sys.stderr, "munki-pkg-make:", pkgDir, "missing build-info file"
     return False
 
 
-def syncPermissions(pkgdir):
-    return subprocess.call(['munkipkg', '--sync', pkgdir])
+def syncPermissions(pkgDir, quiet=False):
+    """Sync the permissions to files/directories from Bom.txt file."""
+    if quiet:
+        return subprocess.call(['munkipkg', '--sync', pkgDir, '--quiet'])
+    else:
+        return subprocess.call(['munkipkg', '--sync', pkgDir])
 
 
-def makePackage(pkgdir):
-    return subprocess.call(['munkipkg', pkgdir])
+def makePackage(pkgDir, quiet=False):
+    """Build munki-pkg package."""
+    if quiet:
+        return subprocess.call(['munkipkg', pkgDir, '--quiet'])
+    else:
+        return subprocess.call(['munkipkg', pkgDir])
+
+
+def movePackage(currentDir, pkgDir, quiet=False):
+    """Move package file from munki-pkg directory to central location."""
+    keepBuild=False
+    
+    # Check for build directory in pkgDir
+    # Retun with error if does not exist
+    pkgDirBuild = os.path.join(pkgDir, 'build')
+    if not os.path.exists(pkgDirBuild):
+        # Is this the best line?
+        print >> sys.stderr, "munki-pkg-make: build directory not found in", pkgDir
+        return
+    
+    # Check for build directory in currentDir
+    # Create directory if does not exist
+    currentDirBuild = os.path.join(currentDir, 'build')
+    if not os.path.exists(currentDirBuild):
+        os.makedirs(currentDirBuild)
+    
+    # Moves all files ending with '.pkg'
+    # Deletes directory and all files
+    for pkg in os.listdir(pkgDirBuild):
+        if pkg.endswith('.pkg'):
+            oldPkg = os.path.join(pkgDirBuild, pkg)
+            newPkg = os.path.join(currentDirBuild, pkg)
+            os.rename(oldPkg, newPkg)
+            if not quiet:
+                print >> sys.stdout, "munki-pkg-make:", pkg, "moved to", currentDirBuild
+    if not keepBuild:
+        shutil.rmtree(pkgDirBuild)
+        if not quiet:
+            print >> sys.stdout, "munki-pkg-make:", pkgDirBuild, "removed"
+    return
 
 
 def main():
+    #
+    mainParser = argparse.ArgumentParser(
+        description='Syncs permissions and then builds multiple packages at a time using the munki-pkg tool.')
+    mainParser.add_argument(
+        '-q',
+        '--quiet',
+        action="store_true",
+        help="Suppress normal output messages. Errors will still be printed to stderr.")
+    mainParser.add_argument(
+        '-d',
+        '--directory',
+        help="Runs munki-pkg-make.py on the given directory. Runs on current directory if none is given.",
+        default='.')
+    args = mainParser.parse_args()
+    
     # Check if script is running as root
     if os.geteuid() != 0:
         print >> sys.stderr, "This script must be run as root"
         return 1
             
     # Check if munkipkg executable is in PATH env
-    exe_path = inPATH('munkipkg')
-    if not exe_path:
+    if not inPath('munkipkg'):
         print >> sys.stderr, "munkipkg not found in PATH"
         return 1
     
-    cd = '.'
-    for d in os.listdir(cd):
-        pkgdir = os.path.join(cd, d)
+    currentDir = args.directory
+    if not os.path.isdir(currentDir):
+        print >> sys.stderr, currentDir, "is not a directory"
+        return 1
         
-        if os.path.isdir(pkgdir):
-            if isPackage(pkgdir):
-                syncPermissions(pkgdir)
-                makePackage(pkgdir)
+    quiet = args.quiet
+    
+    if isPackage(currentDir):
+        pkgDir = currentDir
+        syncPermissions(pkgDir, quiet)
+        makePackage(pkgDir, quiet)
+    else:
+        if not quiet:
+            print >> sys.stdout, "munki-pkg-make: examining directories in", currentDir
+        for subDir in os.listdir(currentDir):
+            pkgDir = os.path.join(currentDir, subDir)
+            if os.path.isdir(pkgDir):
+                if isPackage(pkgDir):
+                    syncPermissions(pkgDir, quiet)
+                    makePackage(pkgDir, quiet)
+                    movePackage(currentDir, pkgDir, quiet)
+    
+    return
 
 
 if __name__ == "__main__":
