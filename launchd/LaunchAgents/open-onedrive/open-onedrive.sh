01f2898f6a09e48f0381f41ebbd12b0fba5301d6
Jacob F. Grant <git@jacobfgrant.com>
2017-11-30 16:17:18 -0800

    Create first launchd LaunchAgent
    Adds the files for a launchd LaunchAgent to ensure OneDrive is open in
    the background (to ensure syncing is enabled). These include the
    LaunchAgent (.plist file) and the job (shell script). This is the first
    launchd LauchDaemon/LaunchAgent in this repository. More will (likely)
    follow.
    
    Going forward, these component files will need to be added to a package.
    One way to do this would be a script which generates the necessary files
    and builds a package using munkipkg.

---
+++ launchd/LaunchAgents/open-onedrive/open-onedrive.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#  open-onedrive.sh
+#  
+#
+#  Created by Jacob F. Grant
+#
+#  Created: 08/04/17
+#  Updated: 11/30/17
+#
+
+if
+    [ -d "/Applications/OneDrive.app" ] &&
+    [ -d /Users/$USER/OneDrive* ] ||
+    [ -d /Users/$USER/Documents/OneDrive* ]
+then
+    open "/Applications/OneDrive.app" -g
+fi
